7152f1a33c92732efc73c126a5fb0ade794e5feb
dronkfc@gmail.com <dronkfc@gmail.com>
2020-10-11 20:21:11 +0300

    Updates

---
+++ precache-manifest.6816136f5ac0f2c6c2a79d7a7cdfac45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565ebe855639feaa86fe31ca13d18944",
+    "url": "/PieChart/index.html"
+  },
+  {
+    "revision": "3413dd9e7bc142bc38fb",
+    "url": "/PieChart/static/css/main.ec0e11dd.chunk.css"
+  },
+  {
+    "revision": "6a1c2037907068afb16a",
+    "url": "/PieChart/static/js/2.d374c70c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PieChart/static/js/2.d374c70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3413dd9e7bc142bc38fb",
+    "url": "/PieChart/static/js/main.4044a011.chunk.js"
+  },
+  {
+    "revision": "3b1390cac1d8fbcd92ce",
+    "url": "/PieChart/static/js/runtime-main.21925869.js"
+  }
+]);
